af652a124c198631be4d963ab837852ceb416413
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-08-11 21:42:25 +0530

    js code

---
+++ Basic/index.js
@@ -0,0 +1,9 @@
+
+
+let number = 10;
+
+for (let i = 0; i < number; i++) {
+    console.log(i);
+}
+
+console.log("All data printed");
---
+++ hello.js
@@ -0,0 +1,9 @@
+
+
+var name = "kunal arya";
+console.log(name);
+
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
