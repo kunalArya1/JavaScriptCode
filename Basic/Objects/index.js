fb3dffcb23882c39dd1bfe7a80e4092e228343d8
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-11-18 22:40:49 +0530

    object started

---
+++ Basic/Objects/index.js
@@ -0,0 +1,10 @@
+const obj = {
+    name :"Kunal Kumar",
+    username : "kunalArya",
+    email : "kunalkrraj@gmail.com"
+};
+
+// Destructuring of Object 
+const { name,username,email} =obj;
+
+console.log(username);
--- hello.js
+++
@@ -1,9 +0,0 @@
-
-
-var name = "kunal arya";
-console.log(name);
-
-
-for (let i = 0; i < 10; i++) {
-    console.log(i);
-}
